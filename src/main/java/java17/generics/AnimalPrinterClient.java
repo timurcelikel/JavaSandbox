64e0af09936dd132b3c29695a6f96c73e8edfd63
Timur Celikel <tcelikel@gmail.com>
2023-04-28 20:41:18 -0700

    added on to generics example

--- src/main/java/java17/generics/AnimalPrinterClient.java
+++ src/main/java/java17/generics/AnimalPrinterClient.java
@@ -1,13 +1,46 @@
 package java17.generics;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AnimalPrinterClient {
 
 	public static void main(String[] args) {
 
-		AnimalPrinter<Dog> dogPrinter = new AnimalPrinter<>(new Dog());
+		AnimalPrinter<Dog> dogPrinter = new AnimalPrinter<>(new Dog("bark"));
 		dogPrinter.print();
 
-		AnimalPrinter<Cat> catPrinter = new AnimalPrinter<>(new Cat());
+		AnimalPrinter<Cat> catPrinter = new AnimalPrinter<>(new Cat("meow"));
 		catPrinter.print();
+
+		shout("yell");
+		shout(23);
+
+		List<Integer> intList = new ArrayList<>();
+		intList.add(36);
+		intList.add(45);
+		printList(intList);
+
+		List<Cat> catList = new ArrayList<>();
+		catList.add(new Cat("meow"));
+		catList.add(new Cat("meeeeow"));
+		printAnimalList(catList);
+	}
+
+	private static <T> void shout(T thingToShout) {
+
+		System.out.println(thingToShout + "!!!");
+	}
+
+	private static void printList(List<?> list) {
+
+		//list.forEach(p -> System.out.println(p));
+		list.forEach(System.out::println);
+	}
+
+	private static void printAnimalList(List<? extends Animal> list) {
+
+		//list.forEach(p -> p.makeNoise());
+		list.forEach(Animal::makeNoise);
 	}
 }
--- src/main/java/java17/generics/Cat.java
+++ src/main/java/java17/generics/Cat.java
@@ -2,9 +2,34 @@ package java17.generics;
 
 public class Cat implements Animal {
 
+	private String noiseToMake;
+
+	public Cat(String noiseToMake) {
+
+		this.noiseToMake = noiseToMake;
+	}
+
+	public String getNoiseToMake() {
+
+		return noiseToMake;
+	}
+
+	public void setNoiseToMake(final String noiseToMake) {
+
+		this.noiseToMake = noiseToMake;
+	}
+
 	@Override
 	public void makeNoise() {
 
-		System.out.println("Meow!");
+		System.out.println(this.noiseToMake);
+	}
+
+	@Override
+	public String toString() {
+
+		return "Cat{" +
+				"noiseToMake='" + noiseToMake + '\'' +
+				'}';
 	}
 }
