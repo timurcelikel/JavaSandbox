6a54f85c82f0b4a0b2c20330ba25877760c6a563
Timur Celikel <tcelikel@gmail.com>
2023-04-28 20:41:18 -0700

    added on to generics example

--- src/main/java/java17/generics/Dog.java
+++ src/main/java/java17/generics/Dog.java
@@ -2,9 +2,34 @@ package java17.generics;
 
 public class Dog implements Animal {
 
+	private String noiseToMake;
+
+	public Dog(String noiseToMake) {
+
+		this.noiseToMake = noiseToMake;
+	}
+
+	public String getNoiseToMake() {
+
+		return noiseToMake;
+	}
+
+	public void setNoiseToMake(final String noiseToMake) {
+
+		this.noiseToMake = noiseToMake;
+	}
+
 	@Override
 	public void makeNoise() {
 
-		System.out.println("Bark!");
+		System.out.println(this.noiseToMake);
+	}
+
+	@Override
+	public String toString() {
+
+		return "Dog{" +
+				"noiseToMake='" + noiseToMake + '\'' +
+				'}';
 	}
 }
