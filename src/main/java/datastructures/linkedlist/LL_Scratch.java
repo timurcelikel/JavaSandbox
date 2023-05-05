fee085d2c34448ae65bb788d87f61564c0a5e688
Timur Celikel <tcelikel@gmail.com>
2023-05-04 21:28:04 -0700

    added prepend to LinkedList excercises

--- src/main/java/datastructures/linkedlist/LL_Scratch.java
+++ src/main/java/datastructures/linkedlist/LL_Scratch.java
@@ -36,34 +36,18 @@ class LinkedList {
         }
     }
 
-    public Node removeLast() {
+    public void prepend(int value) {
+        Node newNode = new Node(value);
         if (length == 0) {
-            System.out.println("List is empty");
-            return null;
-        } else if (length == 1) {
-            Node temp = tail;
-            tail = null;
-            head = null;
-            length = 0;
-            System.out.println("Removed Node: " + temp.value);
-            return temp;
+            head = newNode;
+            tail = newNode;
         } else {
-            // 11 15 23
-            Node temp = tail;
-            tail = head;
-            while (tail.next != null) {
-                temp = tail;
-                tail = tail.next;
-            }
-            Node pre = tail;
-            tail = temp;
-            tail.next = null;
-            length--;
-            System.out.println("Removed Node: " + pre.value);
-            return pre;
+            Node temp = head;
+            newNode.next = head;
+            head = newNode;
+            length++;
         }
     }
-
 
     public void append(int value) {
         Node newNode = new Node(value);
@@ -76,6 +60,32 @@ class LinkedList {
         }
         length++;
     }
+
+    public Node removeLast() {
+        if (length == 0) {
+            System.out.println("List is empty");
+            return null;
+        } else if (length == 1) {
+            Node temp = tail;
+            tail = null;
+            head = null;
+            length = 0;
+            System.out.println("Removed Node: " + temp.value);
+            return temp;
+        } else {
+            Node temp = head;
+            Node pre = head;
+            while (temp.next != null) {
+                pre = temp;
+                temp = temp.next;
+            }
+            tail = pre;
+            tail.next = null;
+            length--;
+            System.out.println("Removed Node: " + temp.value);
+            return temp;
+        }
+    }
 }
 
 public class LL_Scratch {
@@ -85,14 +95,15 @@ public class LL_Scratch {
         myList.append(23);
         myList.append(11);
         myList.printList();
-
         myList.removeLast();
         myList.printList();
-
         myList.removeLast();
         myList.printList();
-
         myList.removeLast();
         myList.removeLast();
+        myList.append(100);
+        myList.prepend(99);
+        myList.prepend(88);
+        myList.printList();
     }
 }
--- src/main/java/datastructures/linkedlist/codingexercises/CodingExercise1.java
+++ src/main/java/datastructures/linkedlist/codingexercises/CodingExercise1.java
@@ -1,14 +1,24 @@
 package datastructures.linkedlist.codingexercises;
 
 public class CodingExercise1 {
+
     public static void main(String[] args) {
 
-        LinkedList myLinkedList = new LinkedList(4);
-        myLinkedList.printAll();
+        LinkedList myList = new LinkedList(10);
+        myList.printList();
+        myList.append(23);
+        myList.append(11);
+        myList.printList();
+        myList.removeLast();
+        myList.printList();
+        myList.removeLast();
+        myList.printList();
+        myList.removeLast();
+        myList.removeLast();
+        myList.append(100);
+        myList.prepend(99);
+        myList.prepend(88);
+        myList.printList();
 
-        myLinkedList.append(23);
-        myLinkedList.append(11);
-        myLinkedList.printList();
-        
     }
 }
