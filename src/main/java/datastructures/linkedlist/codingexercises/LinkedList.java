657f89ae5101ef344cc91678d3d754c898038993
Timur Celikel <tcelikel@gmail.com>
2023-05-04 21:28:04 -0700

    added prepend to LinkedList excercises

--- src/main/java/datastructures/linkedlist/codingexercises/LinkedList.java
+++ src/main/java/datastructures/linkedlist/codingexercises/LinkedList.java
@@ -12,17 +12,6 @@ public class LinkedList {
         length = 1;
     }
 
-    public Node getHead() {
-        return head;
-    }
-
-    public Node getTail() {
-        return tail;
-    }
-
-    public int getLength() {
-        return length;
-    }
 
     public void printAll() {
         System.out.println("Head: " + head.value);
@@ -31,11 +20,28 @@ public class LinkedList {
     }
 
     public void printList() {
-        Node temp = head;        // Start at head
-        System.out.println("Printing List... ");
+        if (length == 0) {
+            System.out.println("List is empty");
+            return;
+        }
+        Node temp = head;
+        System.out.println("Printing list...");
         while (temp != null) {
             System.out.println(temp.value);
-            temp = temp.next;    // Iterate through LL until we are at the end (null)
+            temp = temp.next;
+        }
+    }
+
+    public void prepend(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            Node temp = head;
+            newNode.next = head;
+            head = newNode;
+            length++;
         }
     }
 
@@ -51,12 +57,29 @@ public class LinkedList {
         length++;
     }
 
-    class Node {
-        int value;
-        Node next;
-
-        public Node(final int value) {
-            this.value = value;
+    public Node removeLast() {
+        if (length == 0) {
+            System.out.println("List is empty");
+            return null;
+        } else if (length == 1) {
+            Node temp = tail;
+            tail = null;
+            head = null;
+            length = 0;
+            System.out.println("Removed Node: " + temp.value);
+            return temp;
+        } else {
+            Node temp = head;
+            Node pre = head;
+            while (temp.next != null) {
+                pre = temp;
+                temp = temp.next;
+            }
+            tail = pre;
+            tail.next = null;
+            length--;
+            System.out.println("Removed Node: " + temp.value);
+            return temp;
         }
     }
 }
---
+++ src/main/java/datastructures/linkedlist/codingexercises/Node.java
@@ -0,0 +1,10 @@
+package datastructures.linkedlist.codingexercises;
+
+public class Node {
+    int value;
+    Node next;
+
+    public Node(final int value) {
+        this.value = value;
+    }
+}
