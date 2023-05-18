c6b208d60cd233edae85d62c2f8676c47db0aa80
Timur Celikel <timur@procellahealth.com>
2023-05-18 08:33:45 -0700

    fixed selection sort example

--- src/main/java/datastructures/packt/sorting/SelectionSort.java
+++ src/main/java/datastructures/packt/sorting/SelectionSort.java
@@ -8,15 +8,18 @@ public class SelectionSort {
 		printArray(sortedArray);
 	}
 	public static int[] selectionSort(final int[] array) {
+		int swapCount = 0;
 		for (int i = 0; i < array.length; i++) {
-			for (int k = 0; k < array.length; k++) {
-				if (array[k] > array[i]) {
+			for (int k = i + 1; k < array.length; k++) {
+				if (array[k] < array[i]) {
 					int temp = array[k];
 					array[k] = array[i];
 					array[i] = temp;
+					swapCount++;
 				}
 			}
 		}
+		System.out.println("Swap Count: " + swapCount);
 		return array;
 	}
 
