785640bd3aae254eb6b19b44f977934837b1434f
Timur Celikel <timur.celikel@proton.me>
2023-05-18 10:39:44 -0700

    added bubble sort examples

---
+++ src/main/java/datastructures/packt/sorting/BubbleSort.java
@@ -0,0 +1,58 @@
+package datastructures.packt.sorting;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		int[] array = { 4, 5, 6, 2, 1, 7, 10, 3, 8, 9 };
+		int[] sortedArray = sort(array);
+		System.out.println("Array: " + Arrays.toString(sortedArray));
+	}
+
+	public static int[] sort(final int[] array) {
+		int comparisonCounter = 0;
+		int counter = 0;
+		for (int i = array.length - 1;  i > 0; i--) {
+			int swapCounter = 0;
+			for (int k = array.length - 1; k > counter; k--) {
+				if (array[k - 1] > array[k]) {
+					int temp = array[k];
+					array[k] = array[k - 1];
+					array[k - 1] = temp;
+					swapCounter++;
+				}
+				comparisonCounter++;
+			}
+			counter++;
+			if (swapCounter == 0) {
+				System.out.println("Comparison Counter 1: " + comparisonCounter);
+				return array;
+			}
+		}
+		System.out.println("Comparison Counter 2: " + comparisonCounter);
+		return array;
+	}
+
+	public static int[] videoSort(int[] array) {
+		int comparisonCounter = 0;
+		for (int i = 0; i < array.length; i++) {
+			boolean swapped = false;
+			for (int k = array.length - 1; k > i; k--) {
+				if (array[k] < array[k-1]) {
+					int temp = array[k];
+					array[k] = array[k - 1];
+					array[k - 1] = temp;
+					swapped = true;
+				}
+				comparisonCounter++;
+			}
+			if (!swapped) {
+				System.out.println("Comparison Counter 1: " + comparisonCounter);
+				return array;
+			}
+		}
+		System.out.println("Comparison Counter 2: " + comparisonCounter);
+		return array;
+	}
+}
