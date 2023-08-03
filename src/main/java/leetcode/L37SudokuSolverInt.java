2b2f2db415fa8cb4e65ee2ecd7fe9306a4799e97
Timur Celikel <tcelikel@gmail.com>
2023-08-02 20:35:50 -0700

    Added Leetcode Sudoku Solver Answer

--- src/main/java/leetcode/L37SudokuSolverInt.java
+++ src/main/java/leetcode/L37SudokuSolverInt.java
@@ -1,23 +1,23 @@
 package leetcode;
 
-public class L37SudokuSolver {
+public class L37SudokuSolverInt {
 
 	/* Helpful explanation: https://www.youtube.com/watch?v=mcXc8Mva2bA */
 	public static void main(String[] args) {
 
-		char[][] board = { { '5', '3', '.', '.', '7', '.', '.', '.', '.' },
-				{ '6', '.', '.', '1', '9', '5', '.', '.', '.' },
-				{ '.', '9', '8', '.', '.', '.', '.', '6', '.' },
-				{ '8', '.', '.', '.', '6', '.', '.', '.', '3' },
-				{ '4', '.', '.', '8', '.', '3', '.', '.', '1' },
-				{ '7', '.', '.', '.', '2', '.', '.', '.', '6' },
-				{ '.', '6', '.', '.', '.', '.', '2', '8', '.' },
-				{ '.', '.', '.', '4', '1', '9', '.', '.', '5' },
-				{ '.', '.', '.', '.', '8', '.', '.', '7', '9' } };
+		int[][] board = { { 5, 3, 0, 0, 7, 0, 0, 0, 0 },
+				{ 6, 0, 0, 1, 9, 5, 0, 0, 0 },
+				{ 0, 9, 8, 0, 0, 0, 0, 6, 0 },
+				{ 8, 0, 0, 0, 6, 0, 0, 0, 3 },
+				{ 4, 0, 0, 8, 0, 3, 0, 0, 1 },
+				{ 7, 0, 0, 0, 2, 0, 0, 0, 6 },
+				{ 0, 6, 0, 0, 0, 0, 2, 8, 0 },
+				{ 0, 0, 0, 4, 1, 9, 0, 0, 5 },
+				{ 0, 0, 0, 0, 8, 0, 0, 7, 9 } };
 
 		solveSudoku(board);
 	}
-	public static void solveSudoku(char[][] board) {
+	public static void solveSudoku(int[][] board) {
 		if (solveBoard(board)) {
 			System.out.println("Solved successfully!");
 		} else {
@@ -25,7 +25,7 @@ public class L37SudokuSolver {
 		}
 		printBoard(board);
 	}
-	private static void printBoard(final char[][] board) {
+	private static void printBoard(final int[][] board) {
 		for (int i = 0; i < 9; i++) {
 			for (int k = 0; k < 9; k++) {
 				System.out.print(board[i][k]);
@@ -33,7 +33,7 @@ public class L37SudokuSolver {
 			System.out.println();
 		}
 	}
-	private static boolean isNumberInRow(char[][] board, char number, int row) {
+	private static boolean isNumberInRow(int[][] board, int number, int row) {
 		for (int i = 0; i < 9; i++) {
 			if (board[row][i] == number) {
 				return true;
@@ -41,7 +41,7 @@ public class L37SudokuSolver {
 		}
 		return false;
 	}
-	private static boolean isNumberInColumn(char[][] board, char number, int column) {
+	private static boolean isNumberInColumn(int[][] board, int number, int column) {
 		for (int i = 0; i < 9; i++) {
 			if (board[i][column] == number) {
 				return true;
@@ -49,7 +49,7 @@ public class L37SudokuSolver {
 		}
 		return false;
 	}
-	private static boolean isNumberInBox(char[][] board, char number, int row, int column) {
+	private static boolean isNumberInBox(int[][] board, int number, int row, int column) {
 		// We need to find the top left coordinate of the box
 		int localBoxRow = row - row % 3;
 		int localBoxColumn = column - column % 3;
@@ -62,28 +62,27 @@ public class L37SudokuSolver {
 		}
 		return false;
 	}
-	private static boolean isValidPlacement(char[][] board, char number, int row, int column) {
+	private static boolean isValidPlacement(int[][] board, int number, int row, int column) {
 		// Number is not in row AND number is not in column AND number is not in box
 		return !isNumberInRow(board, number, row) && !isNumberInColumn(board, number, column) &&
 				!isNumberInBox(board, number, row, column);
 	}
-	private static boolean solveBoard(char[][] board) {
+	private static boolean solveBoard(int[][] board) {
 		// The idea here is to start at the first space on the board we need to fill in, find
 		// a candidate value that meets the three criteria and move onto the next square. When we
 		// reach a dead end we backtrack to the previous value and try another value and start again.
 		// We almost kind of defrag the whole board and slowly chip away at it until it's all filled in.
 		for (int row = 0; row < 9; row++) {
 			for (int column = 0; column < 9; column++) {
-				if (board[row][column] == '.') {
+				if (board[row][column] == 0) {
 					// When we find a spot that isn't filled in we start trying number 1-9
 					for (int numberToTry = 1; numberToTry <= 9; numberToTry++) {
-						char charNumber = (char) (numberToTry + '0');
-						if (isValidPlacement(board, charNumber, row, column)) {
-							board[row][column] = charNumber;
+						if (isValidPlacement(board, numberToTry, row, column)) {
+							board[row][column] = numberToTry;
 							if (solveBoard(board)) {
 								return true;
 							} else {
-								board[row][column] = '.';
+								board[row][column] = 0;
 							}
 						}
 					}
